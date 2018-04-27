6c8a39815b7349fd90cf5e3008316c2d252c76d9
Vlad Dumbrava <vdumbrava@starlab.md>
2018-04-27 17:04:36 +0300

    write faker

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -1,15 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vdumbrava
- * Date: 4/27/18
- * Time: 4:14 PM
- */
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
-class CategoryFixtures
+class CategoryFixtures extends Fixture implements OrderedFixtureInterface
 {
 
+    /**
+     * @param ObjectManager $manager
+     *
+     * @return void
+     */
+    public function load(ObjectManager $manager): void
+    {
+
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $category = new Category();
+            $category->setName($faker->word);
+
+            $manager->persist($category);
+
+//            $this->addReference('category', $category);
+
+        }
+
+        $manager->flush();
+
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder(): int
+    {
+        return 1;
+    }
 }
--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -1,15 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vdumbrava
- * Date: 4/27/18
- * Time: 4:14 PM
- */
 
 namespace App\DataFixtures;
 
 
-class JobFixtures
-{
+use App\Entity\Job;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
+class JobFixtures extends Fixture implements OrderedFixtureInterface
+{
+    /**
+     * @param ObjectManager $manager
+     *
+     * @return void
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $job = new Job();
+//            $job->setCategory($manager->$this->getReference('category'));
+
+            $job->setType('full-time');
+            $job->setCompany($faker->company);
+            $job->setLogo($faker->imageUrl());
+            $job->setUrl($faker->url);
+            $job->setPosition($faker->jobTitle);
+//            $location = $faker->country.', '.$faker->city;
+            $job->setLocation($faker->country);
+            $job->setDescription($faker->text(100));
+            $job->setHowToApply($faker->text(30));
+            $job->setPublic($faker->boolean);
+            $job->setActivated($faker->boolean);
+            $job->setToken($faker->text);
+            $job->setEmail($faker->companyEmail);
+            $job->setExpiresAt($faker->dateTime);
+
+            $manager->persist($job);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder(): int
+    {
+        return 2;
+    }
 }
