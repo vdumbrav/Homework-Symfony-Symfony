19b591a79ee0c9a8329a18180893b7912963e2a8
Vlad Dumbrava <vladislav.dumbrava@gmail.com>
2018-04-30 09:36:09 +0300

    create footer header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 var $ = require('jquery');
 
-require( 'datatables.net-responsive-bs4' )();
+require('datatables.net-bs4')();
+require('datatables.net-responsive-bs4')();
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".table").DataTable({
         responsive: {
             details: true
-        }    });
+        }
+    });
 });
