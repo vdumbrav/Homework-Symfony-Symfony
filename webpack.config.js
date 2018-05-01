5cc0cc8b08f3e1e0bb7e5ae0a31efb7e20948e1f
Vlad Dumbrava <vladislav.dumbrava@gmail.com>
2018-05-01 10:46:40 +0300

    lint file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,24 @@
-/* подключим плагин */
+// подключим плагин
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-/* Установим путь куда будет осуществляться сборка */
+     /* Установим путь куда будет осуществляться сборка */
     .setOutputPath('public/build/')
     /* Укажем web путь до каталога web/build */
     .setPublicPath('/build')
     /* Каждый раз перед сборкой будем очищать каталог /build */
     .cleanupOutputBeforeBuild()
-
-   // jquery
-   .autoProvidejQuery()
-
+    // jquery
+    .autoProvidejQuery()
     /* Включим поддержку sass/scss файлов */
     .enableSassLoader()
     /* --- Добавим основной JavaScript в сборку --- */
     .addEntry('js/app', './assets/js/app.js')
-
     /* Добавим наш главный файл ресурсов в сборку */
     .addStyleEntry('css/app', './assets/scss/app.scss')
-
     /* В режиме разработки будем генерировать карту ресурсов */
     .enableSourceMaps(!Encore.isProduction())
+
     .enableVersioning(Encore.isProduction());
 
 /* Экспортируем финальную конфигурацию */
